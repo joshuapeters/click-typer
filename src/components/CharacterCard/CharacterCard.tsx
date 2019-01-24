6990a6407abbe8042d4f4043ac225799c20bb36d
JP <jgpeters717@gmail.com>
2019-01-23 20:40:47 -0500

    added character card

---
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './SelectionArea.css';
+
+export interface State {
+}
+
+export interface Props {
+  character : string,
+  isSelected : boolean
+}
+
+class CharacterCard extends Component<Props, State>  {
+  public render() {
+    const className = this.props.isSelected ? "c-character-card -selected" : "c-character-card"
+    return (
+      <div className = { className }>
+        {
+          this.props.character
+        }
+      </div>
+    );
+  }
+}
+
+export default CharacterCard;
