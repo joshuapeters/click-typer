6f911266fbd316d92b60337349e4e6b021e1bd67
Joshua Peters <jashweiners@localhost.localdomain>
2019-01-24 21:46:26 -0500

    implemented event handlers on main app component

--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,63 @@ export interface State {
   // todo: figure out how to store a reference to the active component
   text?: string,
   updateChildren: boolean;
+  intervalSpeed: number;
+  scrollOffset: any;
+  activeSelection: number;
+  letterList: string[];
 }
 export interface Props {}
 
 class App extends Component<Props, State> {
+
+  private MAX_INTERVAL_SPEED = 250;
+  private MIN_INTERVAL_SPEED = 5000;
 
   constructor(props: Props){
     super(props);
     this.state = {
       text:           undefined,
       updateChildren: false,
+      intervalSpeed:  this.MAX_INTERVAL_SPEED,
+      scrollOffset: {},
+      activeSelection: 0,
+      letterList: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     } as State
+
+    this.onRightClick = this.onRightClick.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  public componentDidMount() {
+    document.onkeydown = this.onKeyPress;
   }
 
   public render() {
     return (
-      <div className="App" onClick = {this.onClick}>
+      <div className="App" onClick = {this.onClick} onContextMenu={this.onRightClick}>
         <div className = "app-container">
           <SelectionArea 
-            charList = { ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] }
-            isActive = { true }
+            charList = { this.state.letterList }
+            isActive = { this.state.activeSelection == 0 }
             onClick = {this.onGlobalClick}
             parentClicked = { this.state.updateChildren }
-            intervalSpeed = { 500 }  
+            intervalSpeed = { this.state.intervalSpeed }  
             />
           <SelectionArea 
             charList = { ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'] }
-            isActive = { false }
+            isActive = { this.state.activeSelection == 1 }
             onClick = {this.onGlobalClick}
             parentClicked = {false}
-            intervalSpeed = { 2000 }  
+            intervalSpeed = { this.state.intervalSpeed }  
             />
           <SelectionArea 
             charList = { ['.',',','?','!','(',')',';','/','&','$','%','@','^','*','_','-','+','=',':','<','>'] }
-            isActive = { false }
+            isActive = { this.state.activeSelection == 2 }
             onClick = {this.onGlobalClick}
             parentClicked = {false}
-            intervalSpeed = { 2000 }  
+            intervalSpeed = { this.state.intervalSpeed }  
             />
-          <textarea className = "c-textbox" placeholder="Please type something..." value = {this.state.text} onChange={this.onTextChange} onClick={this.onTextClick}/>
+          <textarea ref = {input => input && input.focus()}className = "c-textbox" placeholder="Please type something..." autoFocus value = {this.state.text} onChange={this.onTextChange} onClick={this.onTextClick}/>
         </div>
       </div>
     );
@@ -69,16 +87,72 @@ class App extends Component<Props, State> {
     });
   }
 
+  private onRightClick(e: React.MouseEvent<HTMLDivElement>) {
+    e.preventDefault();
+
+    if (this.state.text == undefined)
+      return;
+
+    let text = this.state.text + " ";
+    this.setState({
+      text
+    });
+  }
+
   private onTextChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
     // if text is changed manually, update the global text
     let newText = e.currentTarget.value;
     this.setState({
         text: newText
-    })
+    });
   }
 
   private onTextClick = (e: React.FormEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
+  }
+
+  private onKeyPress(e: any) {
+    switch (e.keyCode) {
+      case '16':
+        this.handleShiftPress();
+        break;
+      case '38':
+      case '40':
+        this.handleArrowPress(e.keyCode);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private handleShiftPress() {
+    let letterList = this.state.letterList;
+    let isCapital = letterList[0] == 'A';
+
+    letterList = letterList.map(c => {
+      return isCapital ? c.toLowerCase() : c.toUpperCase();
+    });
+    
+
+    this.setState({
+      letterList
+    });
+  }
+
+  private handleArrowPress(keyCode: string) {
+    let activeSelection = this.state.activeSelection;
+
+    if (keyCode == '38') {
+        // up arrow
+        activeSelection = activeSelection == 0 ? 2 : activeSelection - 1;
+    }
+    else if (keyCode == '40') {
+        // down arrow
+        activeSelection = activeSelection == 2 ? 0 : activeSelection + 1;
+    }
+    this.setState({
+      activeSelection
+    })
   }
 }
 
